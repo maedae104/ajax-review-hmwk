c85be6d4b360f14391e1289697bf89ee5cf1ed38
Mae Dae <maedae@136-25-136-235.cab.webpass.net>
2022-04-07 18:26:56 -0700

    completed

--- static/ajax-review.js
+++ static/ajax-review.js
@@ -1,9 +1,12 @@
 // PART 1: SHOW A FORTUNE
 
 function showFortune() {
-  // TODO: get the fortune and show it in the fortune-text div
+  fetch('/fortune')
+    .then((response) => response.text())
+    .then((fortune) => {
+      document.querySelector('#fortune-text').innerHTML = fortune;
+    });
 }
-
 document.querySelector('#get-fortune-button').addEventListener('click', showFortune);
 
 // PART 2: SHOW WEATHER
@@ -12,7 +15,12 @@ function showWeather(evt) {
   evt.preventDefault();
   const zipcode = document.querySelector('#zipcode-field').value;
   const url = `/weather?zipcode=${zipcode}`;
-  // TODO: request weather with that URL and show the forecast in #weather-info
+  
+  fetch(url)
+    .then((response) => response.json())
+    .then((jsonData) => {
+      document.querySelector('#weather-info').innerText = jsonData.forecast;
+    }); 
 }
 
 document.querySelector('#weather-form').addEventListener('submit', showWeather);
